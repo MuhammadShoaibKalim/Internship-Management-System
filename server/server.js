3473811895573d0a50b85d427668b24963062fb7
MuhammadShoaibKalim <mmuhammadshoaibkalim@gmail.com>
2026-03-03 14:07:42 +0500

    feat: implement core backend API for the Internship Management System.

--- server/server.js
+++ server/server.js
@@ -1,26 +1,16 @@
-import express from 'express';
 import dotenv from 'dotenv';
-import http from 'http';
-import { Server } from 'socket.io';
-
 dotenv.config();
-const app = express();
+
+import http from 'http';
+import app from './app.js';
+import connectDB from './config/db.js';
+
+
+connectDB();
 
 const PORT = process.env.PORT || 4000;
 const server = http.createServer(app);
 
-
-const io = new Server(server);
-io.on('connection', (socket) => {
-  console.log('a new user connected', socket.id);
-});
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('static', express.static('./public'));
-
-
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`✅ Server listening on port: ${PORT} `);
 });
